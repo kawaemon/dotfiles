b8ffa0ac4ed378f2fb8ae2bb769ff739600f57f2
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-05-19 23:29:29 +0900

    Using alacritty

--- apply.sh
+++ apply.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cp ./tmux.conf ~/.tmux.conf 
+cp ./alacritty.yml ~/.config/alacritty/alacritty.yml
 cp ./{init.vim,plugins.toml,lazy_plugins.toml,coc-settings.json} ~/.config/nvim/
 
--- collect.sh
+++ collect.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cp ~/.tmux.conf ./tmux.conf
+cp ~/.config/alacritty/alacritty.yml .
 cp ~/.config/nvim/{init.vim,plugins.toml,lazy_plugins.toml,coc-settings.json} .
 
