be125447780ea93fd6d8671f19530b35d4b91ca0
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-11-22 00:40:11 +0900

    script: symlinks git config

--- symlink.sh
+++ symlink.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 ln -s $PWD/nvim $HOME/.config/nvim
+ln -s $PWD/home/.gitconfig $HOME/.gitconfig
+ln -s $PWD/home/.gitignore_global $HOME/.gitignore_global
+ln -s $PWD/home/.tmux.conf $HOME/.tmux.conf
