b2a2747999d271fa8b42374ebe9863c1de6b07a8
kawaemon <me@kawaemon.dev>
2022-07-04 21:29:08 +0900

    zsh: temp: fix wrong behavior

--- rc.zsh
+++ rc.zsh
@@ -10,7 +10,7 @@ alias v=nvim
 alias lg=lazygit
 alias vimdiff="nvim -d"
 alias repo='__D=`ghq list | fzf` && cd "$HOME/repo/$__D"'
-alias temp='__D=`mktemp -d /tmp/kXXX` && cd "/tmp/$__D"'
+alias temp='__D=`mktemp -d /tmp/kXXX` && cd "$__D"'
 
 setopt nomatch \
        correct \
