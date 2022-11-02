d626b96a1e80d67a0e148bcc9b174de86d436689
kawaemon <me@kawaemon.dev>
2022-11-02 23:12:31 +0900

    vim: lsp: stop using deprecated capability function

--- nvim/lua/klsp.lua
+++ nvim/lua/klsp.lua
@@ -21,8 +21,7 @@ M.setup = function()
     local lspconfig = require("lspconfig")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
     local init_lsp = function(name, opts)
         lspconfig[name].setup({
