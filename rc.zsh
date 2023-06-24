66ff888d0e735e0fab83ba58eaa934623814879f
kawaemon <me@kawaemon.dev>
2023-06-24 15:14:33 +0900

    zsh: aliases for rfkill

--- rc.zsh
+++ rc.zsh
@@ -12,6 +12,8 @@ alias vimdiff="nvim -d"
 alias repo='__D=`ghq list | fzf` && cd "$HOME/repo/$__D"'
 alias temp='mkdir -p /tmp/ktmp && __D=`mktemp -d /tmp/ktmp/XXX` && cd "$__D"'
 alias gitd='DFT_DISPLAY=inline GIT_EXTERNAL_DIFF=difft git'
+alias airplane='rfkill block all'
+alias unairplane='rfkill unblock all'
 alias npm='corepack npm'
 alias yarn='corepack yarn'
 alias pnpm='corepack pnpm'
