fefb712465076705000011a925121a7a16c04b8f
kawaemon <me@kawaemon.dev>
2023-06-24 15:14:12 +0900

    zsh(node): use corepack

--- rc.zsh
+++ rc.zsh
@@ -12,6 +12,9 @@ alias vimdiff="nvim -d"
 alias repo='__D=`ghq list | fzf` && cd "$HOME/repo/$__D"'
 alias temp='mkdir -p /tmp/ktmp && __D=`mktemp -d /tmp/ktmp/XXX` && cd "$__D"'
 alias gitd='DFT_DISPLAY=inline GIT_EXTERNAL_DIFF=difft git'
+alias npm='corepack npm'
+alias yarn='corepack yarn'
+alias pnpm='corepack pnpm'
 
 setopt nomatch \
        correct \
